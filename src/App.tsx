b28f904a2160528361ded09c8c0073dc0ec46473
Abubeker <Abubekera87@gmail.com>
2024-09-23 12:09:39 -0700

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,7 @@ function App() {
   return (
     <div className="md:snap-y snap-mandatory h-screen overflow-y-scroll">
       <NavBar />
-      {/* <div className=" md:hidden p-2 text-lg text-center justify-center flex items-center fixed z-50 w-full h-screen bg-red-600 text-white font-bold">
-        <p>
-          This Website is not mobile responsive yet, please switch to desktop
-          mode
-        </p>
-      </div> */}
+
       <div className=" px-4 md:px-10 lg:px-20 xl:px-28">
         <Hero />
         <MobileApp />
